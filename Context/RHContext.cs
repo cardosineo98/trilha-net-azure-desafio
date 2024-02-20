63e68a1651f256dc8679e56d973de3a77a5cc1fe
Cardosineo98 <lucasegbl@gmail.com>
2024-02-20 14:46:35 -0300

    Desafio completo

--- Context/RHContext.cs
+++ Context/RHContext.cs
@@ -15,5 +15,14 @@ namespace TrilhaNetAzureDesafio.Context
         }
 
         public DbSet<Funcionario> Funcionarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Funcionario>()
+            .Property(f => f.Salario)
+            .HasColumnType("decimal(18,2)"); // Defina o tipo de dados do SQL Server para decimal(18,2) ou outro que seja apropriado para o seu caso
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
