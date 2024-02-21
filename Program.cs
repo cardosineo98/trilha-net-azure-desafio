640ad8a1c51105a4ec9a2bbc874585654be1a3a1
Lucas Cardoso <99299276+cardosineo98@users.noreply.github.com>
2024-02-20 23:42:32 -0300

    Add files via upload
    Correção de erros, e deploy na azure

--- Program.cs
+++ Program.cs
@@ -1,22 +1,27 @@
+using System.Text.Json.Serialization;
+using desafioazureapi.Context;
 using Microsoft.EntityFrameworkCore;
-using TrilhaNetAzureDesafio.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure the HTTP request pipeline.
 builder.Services.AddDbContext<RHContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Swagger
-app.UseSwagger();
-app.UseSwaggerUI();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
